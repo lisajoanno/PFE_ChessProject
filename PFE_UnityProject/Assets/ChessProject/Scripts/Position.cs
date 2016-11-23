1fcfd48d8418744bd05c01b1c3f7fee470b70124
lisajoanno <jl201213@etu.unice.fr>
2016-11-23 16:42:47 +0100

    Refactor : Position

---
+++ PFE_UnityProject/Assets/ChessProject/Scripts/Position.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+///     The Position of a chess element is :
+///         - The board where it is
+///         - the coordinate of the element on this board
+/// </summary>
+public struct Position
+{
+    public Board board;
+    public Vector2 coo;
+
+    public Position(Board b, Vector2 coo)
+    {
+        this.board = b;
+        this.coo = coo;
+    }
+}
