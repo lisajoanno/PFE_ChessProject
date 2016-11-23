b39b040df33e90fd5f7b454bd2c245d077984ef4
lisajoanno <jl201213@etu.unice.fr>
2016-11-23 11:09:54 +0100

    PNSInnov deleted + Prefabs of the board

--- PFE_UnityProject/Assets/PNSInnov/scripts/observer/IObserver.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections.Generic;
-/// <summary>
-///     The interface describing specifics methods for the Observer in the observer pattern
-/// </summary>
-public interface IObserver{
-	void OnNotify(IList<EventTurn> eventNotify);
-}
