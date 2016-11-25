e0e6bf6a2098b52f169d655e6b3f7229327b26eb
lisajoanno <jl201213@etu.unice.fr>
2016-11-25 11:10:00 +0100

    Refactor : GamePlay folder

---
+++ PFE_UnityProject/Assets/ChessProject/Scripts/GamePlay/MoveController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour {
+
+    //the component managing the teams
+    private TeamTurn teamTurn;
+
+    void Start()
+    {
+        teamTurn = GetComponentInParent<TeamTurn>();
+    }
+
+    public bool Move(Pawn pawn, Square square)
+    {
+        // Light check that the pawn is on the right team
+        if (pawn.Team != teamTurn.CurrentTeamPlaying) Debug.Log("Warning : the pawn you're moving is not of the right team.");
+        
+        
+        //TODO verifier si un autre pion est sur la case
+
+        pawn.Position = square.Position;
+        Board board = pawn.GetComponent<Pawn>().Position.board;
+        Vector2 pos = pawn.GetComponent<Pawn>().Position.coo;
+        // Set of the rigth direction : forward compared to the case (not the board..)
+        pawn.gameObject.transform.transform.up = -(board.GetSquare(pos).gameObject.transform.forward);
+        // Set of the rigth position, on the right board
+        pawn.gameObject.transform.position = board.GetSquare(pos).gameObject.transform.position;
+        // The pawn needs to be on the case
+        pawn.gameObject.transform.position += (pawn.gameObject.transform.transform.up) * 3;
+
+        // The pawn was moved : the team can change
+        teamTurn.ChangeTeam();
+
+        return true;
+    }
+}
---
+++ PFE_UnityProject/Assets/ChessProject/Scripts/GamePlay/TeamTurn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeamTurn : MonoBehaviour {
+
+    // the current team playing
+    // Initialized : 0
+    private int currentTeamPlaying = 0;
+    public int CurrentTeamPlaying
+    {
+        get
+        {
+            return currentTeamPlaying;
+        }
+    }
+
+    /// <summary>
+    /// Method to change the current team playing
+    /// (after a pawn was moved for example).
+    /// </summary>
+    public void ChangeTeam()
+    {
+        if (currentTeamPlaying == 0) currentTeamPlaying = 1;
+        else if (currentTeamPlaying == 1) currentTeamPlaying = 0;
+        else
+        {
+            Debug.Log("The current team playing is neither 0 or 1 ?");
+        }
+    }
+}
